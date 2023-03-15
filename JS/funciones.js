49230e610cd4079765b682b4aaf92d10c8e9183f
Hernán Matías Ortiz <126516854+hortiz57@users.noreply.github.com>
2023-03-15 15:10:23 -0300

    Initial commit

---
+++ JS/funciones.js
@@ -0,0 +1,19 @@
+function confirmacion() {
+    alert("Su mensaje se envió correctamente");
+  }
+
+function redirect() {
+    alert("Se redicionara a otra página")
+}
+
+function agrandarWhapp(tamaño) {
+  document.getElementById('whapp').style.scale = tamaño;
+}
+
+function agrandarLinkedin(tamaño) {
+  document.getElementById('linkedin').style.scale = tamaño;
+}
+
+function agrandarCorreo(tamaño) {
+  document.getElementById('correo').style.scale = tamaño;
+}
